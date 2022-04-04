b0eea3a8d698dc029586606ca1bc6169f34bf617
Laura Pham <l.pham2311@gmail.com>
2022-04-04 22:36:38 +1000

    color flipper project: set up hex color generation using for loop and Math methods to get a random number from array

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// #f15025
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+    // generate a random hex color
+    let hexColor = '#';
+    // create a loop to concatenate 6 values to the hexColor
+    for(let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+})
+
+const getRandomNumber = () => {
+    // Math.random outputs a random number from 0-1
+    // multiply by length of array
+    // use Math.floor to round down to closest integer
+    return Math.floor(Math.random() * (hex.length));
+}
