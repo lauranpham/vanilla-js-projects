cefdf4dbdd24e6e47c6d956d38b57121ab4b1524
Laura Pham <l.pham2311@gmail.com>
2022-04-04 22:29:54 +1000

    color flipper project: set up simple random color generation using dom elements and Math methods

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+    // get random number between 0 and colors.length - 1
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+const getRandomNumber = () => {
+    // Math.random outputs a random number from 0-1
+    // multiply by length of array
+    // use Math.floor to round down to closest integer
+    return Math.floor(Math.random() * (colors.length));
+}
