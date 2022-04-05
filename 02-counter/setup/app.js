63bd856084ed29eab44dbd5419649ee81132501c
Laura Pham <l.pham2311@gmail.com>
2022-04-05 17:34:21 +1000

    counter project: update color for counter value

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -19,6 +19,13 @@ btns.forEach((btn) => {
             count = 0
         }
         value.textContent = count;
+        if (count > 0) {
+            value.style.color = 'green'
+        } else if (count < 0) {
+            value.style.color = 'red'
+        } else {
+            value.style.color = 'black'
+        }
     })
 })
 
