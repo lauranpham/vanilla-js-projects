d46b1e6e06fa1ac234dfe1a81b0ea3497fb77983
Laura Pham <l.pham2311@gmail.com>
2022-04-15 15:07:17 +1000

    pause and play the video via the switch button

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -2,10 +2,16 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 // select switch btn
-const btn = document.querySelector('.switch-btn')
+const btn = document.querySelector('.switch-btn');
 // select video container
-const video = document.querySelector('.video-container')
+const video = document.querySelector('.video-container');
 
 btn.addEventListener('click', () => {
-    btn.classList.toggle('slide')
-})
+	if (!btn.classList.contains('slide')) {
+		btn.classList.add('slide');
+		video.pause();
+	} else {
+		btn.classList.remove('slide');
+		video.play();
+	}
+});
