ea56d620706debe1716b5c5d246bde5098b34c30
Laura Pham <l.pham2311@gmail.com>
2022-04-16 11:23:52 +1000

    toggle show-links class to open links container with set height

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -7,7 +7,14 @@
 const date = document.getElementById('date')
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
 
+navToggle.addEventListener('click', () => {
+    // add show-links class with set height
+    linksContainer.classList.toggle('show-links')
+})
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
