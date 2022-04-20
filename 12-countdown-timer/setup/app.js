f287df22af8950b23582303b185b7d51eba5afa5
Laura Pham <l.pham2311@gmail.com>
2022-04-20 22:29:47 +1000

    use Date methods to display date for timer giveaway

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -26,3 +26,19 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 // can query drilling down multiple selectors
 const items = document.querySelectorAll('.deadline-format h4');
+
+// show dynamic date
+// month follows zero base e.g. may is 4
+let futureDate = new Date(2022, 4, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const date = futureDate.getDate();
+const timeOfDay = hours < 12 ? 'am' : 'pm';
+let month = futureDate.getMonth();
+month = months[month];
+let day = futureDate.getDay();
+day = weekdays[day];
+
+giveaway.textContent = `giveaway ends on ${day} ${date} ${month} ${year} ${hours}:${minutes}${timeOfDay}`;
