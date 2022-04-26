f9a23de41c7b7450f54183ad47eef9d85303354d
Laura Pham <l.pham2311@gmail.com>
2022-04-26 21:47:45 +1000

    add event listener on submit of form and trigger alert when empty string

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -7,10 +7,37 @@ const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 // edit option
+let editElement; //default to undefined
+let editFlag = false;
+let editId = '';
+// ****** FUNCTIONS **********
+const displayAlert = (message, type) => {
+    alert.textContent = message;
+    alert.classList.add(`alert-${type}`);
+    setTimeout(() => {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${type}`);    
+    }, 5000)
+}
+
+const addItem = (e) => {
+	e.preventDefault();
+	const value = grocery.value;
+	// create a unique id with the current date time
+	const id = Date.now().toString();
+	if (value && !editFlag) {
+		console.log('add item to list');
+	} else if (value && editFlag) {
+		console.log('edit item');
+	} else {
+        displayAlert('empty alert', 'danger')
+	}
+};
+
 
 // ****** EVENT LISTENERS **********
-
-// ****** FUNCTIONS **********
+// submit form
+form.addEventListener('submit', addItem);
 
 // ****** LOCAL STORAGE **********
 
