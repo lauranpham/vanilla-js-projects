f1d47fc20b46b37d9b93e8eda75a7a396a10b809
Laura Pham <l.pham2311@gmail.com>
2022-05-05 22:10:47 +1000

    move carousel with next and prev btn clicks

--- 15-slider/setup/app.js
+++ 15-slider/setup/app.js
@@ -5,3 +5,22 @@ const prevBtn = document.querySelector('.prevBtn')
 slides.forEach((slide, index) => {
     slide.style.left = `${index * 100}%`
 })
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+    counter++;
+    updateCarousel()
+})
+
+prevBtn.addEventListener('click', () => {
+    counter--;
+    updateCarousel()
+})
+
+// translate style depending on counter value 
+const updateCarousel = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
