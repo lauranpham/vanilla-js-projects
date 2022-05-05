1d77bf3e026b8830480cd0a9f284b0db33a587d9
Laura Pham <l.pham2311@gmail.com>
2022-05-05 22:14:37 +1000

    handle moving carousel beyond the end and before start

--- 15-slider/setup/app.js
+++ 15-slider/setup/app.js
@@ -20,6 +20,12 @@ prevBtn.addEventListener('click', () => {
 
 // translate style depending on counter value 
 const updateCarousel = () => {
+    if (counter === slides.length) {
+        counter = 0
+    }
+    if (counter < 0 ) {
+        counter = (slides.length - 1)
+    }
     slides.forEach((slide) => {
         slide.style.transform = `translateX(-${counter * 100}%)`
     })
